69cb9ac79b3ddcaddc4ecdce694dbec2fbb57010
jdeeny <jdeeny@gmail.com>
2019-01-16 14:30:11 -0700

    no ripple

--- main.lua
+++ main.lua
@@ -1,11 +1,8 @@
-local ripple = require 'lib.ripple'
-
 
 function love.load()
   local s = love.audio.newSource('assets/untitled.ogg', 'static')
-  local r = ripple.newSound({source = s, tags = { }})
-  r:setLooping(true)
-  r:play()
+  s:setLooping(true)
+  s:play()
 
 end
 
